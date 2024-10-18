02c507b5b78795a45f30dcaf7815871fb93e7091
nyakiomaina <carolinenyakio21@gmail.com>
2024-10-18 16:18:47 +0300

    sample foundry contracts

--- src/CoprocessorCaller.sol
+++ src/CoprocessorCaller.sol
@@ -4,30 +4,32 @@ pragma solidity ^0.8.0;
 import "./ICoprocessor.sol";
 
 contract CoprocessorCaller {
-    ICoprocessor public coprocessor;
-    bytes32 public machineHash;
+    ICoprocessor public immutable coprocessor;
+    bytes32 public immutable machineHash;
 
     bytes public lastResult;
 
-    address public coprocessorAddress;
+    event ResultReceived(bytes output);
 
     constructor(address _coprocessorAddress, bytes32 _machineHash) {
+        require(_coprocessorAddress != address(0), "Invalid coprocessor address");
         coprocessor = ICoprocessor(_coprocessorAddress);
-        coprocessorAddress = _coprocessorAddress;
         machineHash = _machineHash;
     }
 
+    /// @notice Calls the coprocessor to issue a task
+    /// @param input The input data for the coprocessor
     function callCoprocessor(bytes calldata input) external {
         coprocessor.issueTask(machineHash, input, address(this));
     }
 
+    /// @notice Callback function for the coprocessor to send back results
+    /// @param output The output data from the coprocessor
     function onResult(bytes calldata output) external {
-        require(msg.sender == coprocessorAddress, "Unauthorized caller");
+        require(msg.sender == address(coprocessor), "Unauthorized caller");
 
         lastResult = output;
 
         emit ResultReceived(output);
     }
-
-    event ResultReceived(bytes output);
 }
