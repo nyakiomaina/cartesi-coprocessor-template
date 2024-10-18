52e072b52225a175158f573126dea74e39dd42c5
nyakiomaina <carolinenyakio21@gmail.com>
2024-10-18 16:40:55 +0300

    sample foundry contracts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,13 +6,11 @@ import "../src/CoprocessorCaller.sol";
 
 contract DeployScript is Script {
     function run() external {
-        // Load environment variables
         address coprocessorAddress = vm.envAddress("COPROCESSOR_ADDRESS");
         bytes32 machineHash = vm.envBytes32("MACHINE_HASH");
 
         vm.startBroadcast();
 
-        // Deploy the contract
         CoprocessorCaller caller = new CoprocessorCaller(coprocessorAddress, machineHash);
 
         console.log("CoprocessorCaller deployed at:", address(caller));
